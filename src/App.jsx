09adcf30cd5ecdc8fb37f5763cdf522b1e72464c
Cleve-keem <belloharyourmidey@gmail.com>
2024-12-10 12:40:13 -0800

    eat-n-split

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+import "./App.css";
+import FriendsList from "./Components/FriendsList";
+
+function App() {
+  return (
+    <div>
+      <FriendsList />
+    </div>
+  );
+}
+
+export default App;
